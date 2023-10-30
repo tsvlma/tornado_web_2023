0889f582e62b3fb4140bb682879cfd9388adbfc5
Tornado-11 <tornado-11@Admin-11s-iMac.local>
2023-10-30 17:41:26 +0800

    add:lesson days

---
+++ Lesson-Day-17/js/scripts.js
@@ -0,0 +1,42 @@
+console.log('lesson day 17 - js conditionals')
+  
+// variable
+var myName = 'john' ; // string
+var myAge = 25; //number (int)
+var myJob = 'teacher'; // string
+
+
+let unen = true; //boolean true variable
+let hudal = false // bolean false variable
+
+if(hudal){
+    console.log('you are telling me the truth');
+} else{
+    console.log('you are telling me the lie');
+}
+
+let age = 68
+
+if ( age > 13 && age < 19 ){
+    console.log('you are a teenager!')
+} else if ( age > 19 && age < 65){
+    console.log('you are an adult!')
+} else {
+    console.log('log are senior!')
+}
+
+
+
+
+
+// loops 
+for ( let i = 0; i <10; i++){
+    console.log(i)
+}
+
+
+let i = 0;
+while(i < 10) {
+    console.log(i);
+    i++
+}
