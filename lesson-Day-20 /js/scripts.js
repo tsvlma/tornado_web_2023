0e22fec8f591162368f182a0917de37411c7ebb1
Tornado-11 <tornado-11@Admin-11s-iMac.local>
2023-11-01 15:32:09 +0800

    add:lesson day 20

---
+++ lesson-Day-20 /js/scripts.js
@@ -0,0 +1,25 @@
+console.log('lesson day 20 - js functions, functions in html ');
+
+// function declaration  todorhoilno
+function sayHello(){
+    console.log('hello');
+}
+// function cal of function declgaration
+
+sayHello();
+
+
+// function expression 
+const sayHello2 = function (){
+    console.log('hello');
+}
+
+sayHello();
+
+const PI = 3.15;
+console.log(PI);
+
+
+const sayHello3 = () =>{
+    console.log
+}
